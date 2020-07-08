01889332c41123aba3f1a1326bb1f8ca7c0c71c9
Craig Worthington <craigworthington33@gmail.com>
2020-07-08 17:02:24 +0100

    created demo program with script

---
+++ bindings/python/scrolling-panel/solid-colour.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+from panelbase import PanelBase
+import time
+
+class SimpleSquare(PanelBase):
+    def __init__(self, *args, **kwargs):
+        super(SimpleSquare, self).__init__(*args, **kwargs)
+
+    def run(self):
+        offset_canvas = self.matrix.CreateFrameCanvas()
+
+        # fill the canvas red
+        offset_canvas.Fill(255, 0, 0)
+        offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
+        time.sleep(2)
+
+        # Fill the canvas green
+        offset_canvas.Fill(0, 255, 0)
+        offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
+        time.sleep(2)
+
+        # Fill the canvas blue
+        offset_canvas.Fill(0, 0, 255)
+        offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
+        time.sleep(2)
+        
+        # while True:
+        #     for x in range(0, self.matrix.width):
+        #         offset_canvas.SetPixel(x, x, 255, 255, 255)
+        #         offset_canvas.SetPixel(offset_canvas.height - 1 - x, x, 255, 0, 255)
+
+        #     for x in range(0, offset_canvas.width):
+        #         offset_canvas.SetPixel(x, 0, 255, 0, 0)
+        #         offset_canvas.SetPixel(x, offset_canvas.height - 1, 255, 255, 0)
+
+        #     for y in range(0, offset_canvas.height):
+        #         offset_canvas.SetPixel(0, y, 0, 0, 255)
+        #         offset_canvas.SetPixel(offset_canvas.width - 1, y, 0, 255, 0)
+        #     offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
+
+
+# Main function
+if __name__ == "__main__":
+    simple_square = SimpleSquare()
+    if (not simple_square.process()):
+        simple_square.print_help()
